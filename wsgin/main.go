ecee200f8b4019d78e5b882da4799a967bdc5254
AriAlanPR <shaymin2003@gmail.com>
2023-10-17 17:01:21 -0500

    added first examples

---
+++ wsgin/main.go
@@ -0,0 +1,10 @@
+package main
+
+// managing gin web server from tutorial https://go.dev/doc/tutorial/web-service-gin
+// album represents data about a record album.
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
