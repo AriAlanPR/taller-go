14ec57c92a97421f34ed91897846ff9bd85cb203
AriAlanPR <shaymin2003@gmail.com>
2023-10-21 02:52:22 -0500

    added new project layout with view template rendered

---
+++ tecapp/main.go
@@ -0,0 +1,30 @@
+//main
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func main() {
+	// set the router
+	router = gin.Default()
+
+	// Process the templates at the start so that they don't have to be loaded
+  	// from the disk again. This makes serving HTML pages very fast.
+  	router.LoadHTMLGlob("view/layout/*")
+	
+	//define main digimon route
+	router.GET("/digimon", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			gin.H{"title":"TecApp"},
+		)
+	})
+
+	//Start and run the server
+	router.Run("localhost:3000")
+}
