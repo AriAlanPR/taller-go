5df2e259b4f9ebeede55f32a15074db2fc130b92
AriAlanPR <shaymin2003@gmail.com>
2023-10-24 16:34:56 -0500

    added example of converting number types to string

--- salarycalc/main.go
+++ salarycalc/main.go
@@ -5,9 +5,18 @@ import (
 	"salarycalc/basic/gross"
 
 	"fmt"
+	"strconv"
 )
+
+type float float64
+func (fl float) toString() string {
+	return strconv.FormatFloat(float64(fl), 'f', -1, 64)
+}
 
 func main() {
 	b.Basic = 10000
+	f := float(b.Basic)
+	str := f.toString()
 	fmt.Println(gross.GrossSalary())
+	fmt.Println("Basic salary: " + str)
 }
